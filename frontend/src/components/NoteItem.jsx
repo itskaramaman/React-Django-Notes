47d37e6fd516bd841a36d4a7c50d7d3e2058a535
itskaramaman <singhkaramjeetaman@gmail.com>
2023-04-09 16:50:54 -0700

    note item ui updated

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,10 +1,25 @@
 import { Link } from "react-router-dom";
 
+const getTitle = (note) => {
+  let title = note.body.split("\n")[0];
+  if (title.length > 45) {
+    return title.slice(0, 45);
+  }
+  return title;
+};
+
+const getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
 const NoteItem = ({ note }) => {
   return (
     <Link to={`/notes/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note?.body}</h3>
+        <h3>{getTitle(note)}</h3>
+        <p>
+          <span>{getTime(note)}</span>
+        </p>
       </div>
     </Link>
   );
